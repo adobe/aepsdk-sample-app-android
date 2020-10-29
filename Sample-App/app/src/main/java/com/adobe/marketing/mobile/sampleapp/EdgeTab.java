65ee9db9b603a82713b812317722a846f1488dd3
Kevin Lind <lind@adobe.com>
2020-10-29 16:22:52 -0700

    Update EdgeTab, use product spinner for all examples.

--- Sample-App/app/src/main/java/com/adobe/marketing/mobile/sampleapp/EdgeTab.java
+++ Sample-App/app/src/main/java/com/adobe/marketing/mobile/sampleapp/EdgeTab.java
@@ -9,9 +9,12 @@
 
 package com.adobe.marketing.mobile.sampleapp;
 
+import android.content.Context;
 import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
@@ -23,6 +26,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RatingBar;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.adobe.marketing.mobile.AdobeCallback;
 import com.adobe.marketing.mobile.Edge;
@@ -35,6 +39,7 @@ import com.adobe.marketing.mobile.xdm.Commerce;
 import com.adobe.marketing.mobile.xdm.MobileSDKCommerceSchema;
 import com.adobe.marketing.mobile.xdm.Order;
 import com.adobe.marketing.mobile.xdm.PaymentsItem;
+import com.adobe.marketing.mobile.xdm.ProductListAdds;
 import com.adobe.marketing.mobile.xdm.ProductListItemsItem;
 import com.adobe.marketing.mobile.xdm.ProductReviews;
 import com.adobe.marketing.mobile.xdm.Purchases;
@@ -42,6 +47,7 @@ import com.adobe.marketing.mobile.xdm.RatingEnum;
 import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -51,6 +57,13 @@ import java.util.Objects;
  * create an instance of this fragment.
  */
 public class EdgeTab extends Fragment implements NavigationAware {
+
+    private final ProductItem[] products = new ProductItem[] {
+        new ProductItem("SHOES123", "Red canvas shoes", 34.76, "USD"),
+        new ProductItem("SHOES456", "Brown leather shoes", 52.81, "USD"),
+        new ProductItem("HAT567", "Wool Hat", 25.15, "USD"),
+        new ProductItem("HAT089", "Straw Hat", 11.85, "USD")
+    };
 
     public EdgeTab() {
         // Required empty public constructor
@@ -76,10 +89,9 @@ public class EdgeTab extends Fragment implements NavigationAware {
             public void onClick(View v) {
                 sendAddToCartXdmEvent();
 
-                // Remove toast message once sendAddToCartXdmEvent is implemented
                 Resources res = getResources();
                 View view = getView().findViewById(R.id.layoutMain);
-                Snackbar.make(view, res.getString(R.string.add_to_cart_not_implemented_message), Snackbar.LENGTH_SHORT)
+                Snackbar.make(view, res.getString(R.string.add_to_cart_message), Snackbar.LENGTH_SHORT)
                         .show();
             }
         });
@@ -108,12 +120,11 @@ public class EdgeTab extends Fragment implements NavigationAware {
                 EditText productReviewEditText = getView().findViewById(R.id.text_review_text);
 
                 String reviewerId = reviewerEmailEditText.getText().toString();
-                String productSku = (String)productsSpinner.getSelectedItem();
+                ProductItem product = (ProductItem)productsSpinner.getSelectedItem();
                 int rating = (int)ratingBar.getRating();
                 String productReview = productReviewEditText.getText().toString();
 
-
-                sendProductReviewXdmEvent(reviewerId, productSku, rating, productReview);
+                sendProductReviewXdmEvent(product, reviewerId, rating, productReview);
 
                 Resources res = getResources();
                 View view = getView().findViewById(R.id.layoutMain);
@@ -124,16 +135,8 @@ public class EdgeTab extends Fragment implements NavigationAware {
         });
 
         // Setup product list spinner
-        ArrayList<String> arrayList = new ArrayList<String>() {{
-            add("SHOES123");
-            add("SHOES456");
-            add("HAT567");
-            add("HAT089");
-        }};
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(Objects.requireNonNull(getActivity()), android.R.layout.simple_spinner_item, arrayList);
-        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         Spinner spinner = view.findViewById(R.id.products_spinner);
-        spinner.setAdapter(arrayAdapter);
+        spinner.setAdapter(new SpinnerAdapter(Objects.requireNonNull(getActivity()), android.R.layout.simple_spinner_item, products));
     }
 
     @Override
@@ -147,28 +150,60 @@ public class EdgeTab extends Fragment implements NavigationAware {
     }
 
     private void sendAddToCartXdmEvent() {
-        // TODO to be implemented
+        Spinner productsSpinner = getView().findViewById(R.id.products_spinner);
+        ProductItem product = (ProductItem) productsSpinner.getSelectedItem();
+
+        /// Create list with the added items
+        final ProductListItemsItem product1 = new ProductListItemsItem();
+        product1.setName(product.name);
+        product1.setPriceTotal(product.price);
+        product1.setSKU(product.sku);
+        product1.setQuantity(1);
+        product1.setCurrencyCode(product.currencyCode);
+
+        List<ProductListItemsItem> productItemsList = new ArrayList<ProductListItemsItem>() {{
+            add(product1);
+        }};
+
+        ProductListAdds productListAdds = new ProductListAdds();
+        productListAdds.setValue(1);
+
+        /// Create Commerce object and add ProductListAdds details
+        Commerce commerce = new Commerce();
+        commerce.setProductListAdds(productListAdds);
+
+        // Compose the XDM Schema object and set the event name
+        MobileSDKCommerceSchema xdmData = new MobileSDKCommerceSchema();
+        xdmData.setEventType("commerce.productListAdds");
+        xdmData.setCommerce(commerce);
+        xdmData.setProductListItems(productItemsList);
+
+        // Create an Experience Event with the built schema and send it using the AEP Edge extension
+        ExperienceEvent event = new ExperienceEvent.Builder()
+                .setXdmSchema(xdmData)
+                .build();
+        Edge.sendEvent(event, new EdgeCallback() {
+            @Override
+            public void onResponse(Map<String, Object> data) {
+                Log.d("Send XDM Event", String.format("Received response for event 'commerce.productListAdds': %s", data));
+            }
+        });
     }
 
     private void sendPurchaseXdmEvent() {
+        Spinner productsSpinner = getView().findViewById(R.id.products_spinner);
+        ProductItem product = (ProductItem) productsSpinner.getSelectedItem();
+
         /// Create list with the purchased items
         final ProductListItemsItem product1 = new ProductListItemsItem();
-        product1.setName("Shoes");
-        product1.setPriceTotal(34.76);
-        product1.setSKU("SHOES123");
+        product1.setName(product.name);
+        product1.setPriceTotal(product.price);
+        product1.setSKU(product.sku);
         product1.setQuantity(1);
-        product1.setCurrencyCode("USD");
-
-        final ProductListItemsItem product2 = new ProductListItemsItem();
-        product2.setName("Hat");
-        product2.setPriceTotal(15.3);
-        product2.setSKU("HAT567");
-        product2.setQuantity(2);
-        product2.setCurrencyCode("USD");
+        product1.setCurrencyCode(product.currencyCode);
 
         List<ProductListItemsItem> purchasedItems = new ArrayList<ProductListItemsItem>() {{
             add(product1);
-            add(product2);
         }};
 
         double orderTotal = 0D;
@@ -211,32 +246,59 @@ public class EdgeTab extends Fragment implements NavigationAware {
         Edge.sendEvent(event, new EdgeCallback() {
             @Override
             public void onResponse(Map<String, Object> data) {
-                Log.d("Send Purchase XDM Event", String.format("Received response for event 'commerce.purchases': %s", data));
+                Log.d("Send XDM Event", String.format("Received response for event 'commerce.purchases': %s", data));
             }
         });
     }
 
-    private void sendProductReviewXdmEvent(final String reviewerId, final String productId, final int rating, final String text) {
+    private void sendProductReviewXdmEvent(final ProductItem product, final String reviewerId, final int rating, final String text) {
+        // TODO - Assignment 3
+    }
 
-        Acopprod3 review = new Acopprod3();
-        review.setProductId(productId);
-        review.setReviewerId(reviewerId);
-        review.setReviewText(text);
-        review.setRating(RatingEnum.values()[rating-1]);
+    private static class ProductItem {
+        final String sku;
+        final String name;
+        final double price;
+        final String currencyCode;
 
-        ProductReviews xdmData = new ProductReviews();
-        xdmData.setAcopprod3(review);
-        xdmData.setEventType("product.review");
+        public ProductItem(final String sku, final String name, final double price, final String currencyCode) {
+            this.sku = sku;
+            this.name = name;
+            this.price = price;
+            this.currencyCode = currencyCode;
+        }
+    }
 
-        // Create an Experience Event with the built schema and send it using the AEP Edge extension
-        ExperienceEvent event = new ExperienceEvent.Builder()
-                .setXdmSchema(xdmData.serializeToXdm(), MainApp.DATASET_ID)
-                .build();
-        Edge.sendEvent(event, new EdgeCallback() {
-            @Override
-            public void onResponse(Map<String, Object> data) {
-                Log.d("Send Purchase XDM Event", String.format("Received response for event 'commerce.purchases': %s", data));
-            }
-        });
+    private static class SpinnerAdapter extends ArrayAdapter<ProductItem> {
+        private final ProductItem[] products;
+
+        public SpinnerAdapter(@NonNull Context context, int resource, @NonNull ProductItem[] objects) {
+            super(context, resource, objects);
+            products = objects;
+        }
+
+        @Override
+        public int getCount() {
+            return products.length;
+        }
+
+        @Override
+        public ProductItem getItem(int position) {
+            return products[position];
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            TextView label = (TextView) super.getView(position, convertView, parent);
+            label.setText(products[position].name);
+            return label;
+        }
+
+        @Override
+        public View getDropDownView(int position, View convertView, ViewGroup parent) {
+            TextView label = (TextView) super.getDropDownView(position, convertView, parent);
+            label.setText(products[position].name);
+            return label;
+        }
     }
 }
